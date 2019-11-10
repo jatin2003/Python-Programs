293cac72e3a0b86eb055bb540a16c48abcca7f14
jatinnr <56562571+jatinnr@users.noreply.github.com>
2019-11-10 09:28:38 +0530

    Add files via upload

---
+++ Square.py
@@ -0,0 +1,17 @@
+# Progra to draw Square using Turtle
+
+import turtle
+
+win = turtle.Turtle()
+
+win.color("blue","skyblue")
+
+win.begin_fill()
+win.forward(100)
+win.setheading(90)
+win.forward(100)
+win.setheading(180)
+win.forward(100)
+win.setheading(270)
+win.forward(100)
+win.end_fill()
