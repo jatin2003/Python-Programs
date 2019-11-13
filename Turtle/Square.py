2b8f10864c3ccb4e1c2b82ccb176ff5a3e4f1c2c
jatinnr <56562571+jatinnr@users.noreply.github.com>
2019-11-13 17:45:15 +0530

    Create Square.py

---
+++ Turtle/Square.py
@@ -0,0 +1,17 @@
+# Program to draw Square using Turtle
+
+import turtle
+
+win = turtle.Turtle()
+
+win.color("blue","cyan")
+
+win.begin_fill()
+win.forward(100)
+win.setheading(90)
+win.forward(100)
+win.setheading(180)
+win.forward(100)
+win.setheading(270)
+win.forward(100)
+win.end_fill()
